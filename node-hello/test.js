51e0514f9a5496a1e53f22448adf5ddd7c42c504
Goda Attila <attila.goda@gmail.com>
2019-07-06 16:32:29 +0200

    initial commit

---
+++ node-hello/test.js
@@ -0,0 +1,20 @@
+
+function egyik() {
+    return {
+      a: 10
+    }
+  }
+
+  function masik()
+  {
+    return
+    {
+      a: 10
+    }
+  }
+
+
+console.log(egyik(),masik());
+
+console.log({a:10});
+  
